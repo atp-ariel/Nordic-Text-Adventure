363243c2f1e2db5c6db9b164a07dd9afdbeacf54
CTS-crypto <72420685+CTS-crypto@users.noreply.github.com>
2021-06-25 19:10:25 -0400

    new update

--- src/Proyecto.hs
+++ src/Proyecto.hs
@@ -10,9 +10,15 @@ main = do
     passage <- obtainPassage
     putStrLn (text passage)
     unless (length (nextPossiblePassages passage)==0) $ do
-    sentence <- getLine
-    unless ((map toLower sentence)=="salir") $ do
-    let auxPassage = next passage (endBy " " sentence) 
-    updatePassage passage auxPassage
-    main
+        sentence <- getLine
+        unless ((map toLower sentence)=="salir") $ do
+            putStrLn ""
+            let auxPassage = next passage (endBy " " sentence) 
+            if (id auxPassage) == (id passage) then do
+                putStrLn "Acción inválida o irreconocible. Quizás faltan palabras o la acción no es esperada. Chequea también la ortografía de las palabras. No avance"
+                main
+            else do
+                updatePassage passage auxPassage
+                main
+    
     
