a679ca4cd19a4c2387358d13988501123a0ec585
ArielTriana <usich37@gmail.com>
2021-06-26 17:11:06 -0400

    Refactoring

--- src/Functions.hs
+++ src/Functions.hs
@@ -5,25 +5,29 @@ module Functions
     updatePassage
 ) where
 
+import Synonyms
 import Passages
 import Data.IORef
 import Data.Char
 import System.IO.Unsafe(unsafePerformIO)
 
-find :: String->[String]->Bool
+-- Find a word in a list of words
+find :: String -> [String] -> Bool
 find word [] = False
 find word (x:xs) | word == (map toLower x) = True
                  | otherwise = find word xs 
 
-identifyKeywords :: Passage->[String]->Bool
+-- Identify keywords of a passage in a list of words
+identifyKeywords :: Passage -> [String] -> Bool
 identifyKeywords passage words = all (\x -> find x words) (keywords passage)    
 
-next::Passage->[String]->Passage
+-- Get the next passage 
+next :: Passage -> [String] -> Passage
 next passage words | length list == 0 = passage
                    | otherwise = list !! 0
                    where list = filter (\x -> identifyKeywords x words) (nextPossiblePassages passage)
 
-actualPassage::IORef Passage
+actualPassage :: IORef Passage
 actualPassage = unsafePerformIO(newIORef inicialPassage)
 {-# NOINLINE actualPassage #-}
 
@@ -36,3 +40,7 @@ obtainPassage = readIORef actualPassage
 updatePassage :: Passage -> Passage -> IO Passage
 updatePassage actualP nextP | nextP == actualP = (changePassage actualP)
                             | otherwise = (changePassage nextP)
+
+-- Return a list of all synonyms of passage's keywords
+getSym :: Passage -> [[String]]
+getSym passage = filter () (map (\x -> sym x) allSynonyms)
