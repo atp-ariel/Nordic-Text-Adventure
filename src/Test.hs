3d3ccee819933e44194234d94ca89ddf60134325
ArielTriana <usich37@gmail.com>
2021-06-27 22:42:10 -0400

    Finish tester

--- src/Test.hs
+++ src/Test.hs
@@ -2,10 +2,13 @@ import Functions
 import Passages
 import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
+import Control.Monad
+import System.IO
+import Data.IORef
 
 main = defaultMain test
 
-test = testGroup "================================\n||                            ||\n||  Loki Text Adventure Test  ||\n||                            ||\n================================" [findTests, symTests]
+test = testGroup "================================\n||                            ||\n||  Loki Text Adventure Test  ||\n||                            ||\n================================" [findTests, symTests, managerPassagesTests]
 
 -- Find Operation test
 findTests = testGroup "Find unit test" [emptyStringTest, emptyListTest, foundUpTest, foundUpCaseTest, foundUpOnUpperTest]
@@ -21,3 +24,8 @@ symTests = testGroup "Synonyms unit test" [emptyPassageTest,roomSymTest]
 
 emptyPassageTest = testCase "Passage with empty keywords" $ assertEqual [] [] (getSym inicialPassage) 
 roomSymTest = testCase "Get synonyms of RoomPassages's keywords" $ assertEqual [] [["entrar", "ingresar", "pasar", "acceder", "adentrarse"],["casa", "residencia", "vivienda", "domicilio", "morada", "choza", "tienda"]] (getSym  (head (nextPossiblePassages inicialPassage)))
+
+-- Change passages
+managerPassagesTests = testGroup "Change passage unit test" [changeTest]
+
+changeTest = testCase "Change to the next passage" $ assertEqual [] (pid $ head (nextPossiblePassages inicialPassage)) (pid $ next inicialPassage ["entrar", "a", "la", "casa"])
